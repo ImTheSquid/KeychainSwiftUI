1486264871ff8c8b57a84f959dc0e2a39a4747f1
Jack Hogan <jackhogan11@gmail.com>
2024-09-30 14:16:18 -0400

    Initial commit

--- Package.swift
+++ Package.swift
@@ -5,20 +5,30 @@ import PackageDescription
 
 let package = Package(
     name: "KeychainSwiftUI",
+    platforms: [
+        .macOS(.v10_15),
+        .iOS(.v13),
+        .watchOS(.v6),
+        .tvOS(.v13)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "KeychainSwiftUI",
             targets: ["KeychainSwiftUI"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/evgenyneu/keychain-swift", .upToNextMajor(from: "24.0.0")),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "KeychainSwiftUI"),
+            name: "KeychainSwiftUI", dependencies: [.product(name: "KeychainSwift", package: "keychain-swift")]),
         .testTarget(
             name: "KeychainSwiftUITests",
             dependencies: ["KeychainSwiftUI"]
         ),
     ]
+
 )
--- Sources/KeychainSwiftUI/KeychainSwiftUI.swift
+++ Sources/KeychainSwiftUI/KeychainSwiftUI.swift
@@ -1,2 +1,92 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import Combine
+import SwiftUI
+import KeychainSwift
+
+private final class PublisherObservableObject: ObservableObject {
+    var subscriber: AnyCancellable?
+    
+    init(publisher: AnyPublisher<Void, Never>) {
+        subscriber = publisher.sink(receiveValue: { [weak self] _ in
+            self?.objectWillChange.send()
+        })
+    }
+}
+
+@MainActor
+fileprivate let keychainSubject = PassthroughSubject<String, Never>()
+
+@propertyWrapper
+public struct KeychainStorage<Value: Codable & Equatable & Sendable>: DynamicProperty {
+    @ObservedObject private var observer: PublisherObservableObject
+    private let key: String
+    private let chain: KeychainSwift
+    @State private var value: Value?
+   
+    @MainActor
+    init(wrappedValue: Value? = nil, _ key: String, accessGroup: String? = nil) {
+        self.key = key
+        self.chain = KeychainSwift()
+        self.observer = .init(publisher: keychainSubject.filter { k in k == key }.map { _ in () }.eraseToAnyPublisher())
+        if let accessGroup = accessGroup {
+            self.chain.accessGroup = accessGroup
+        }
+        
+        // Attempt to load keychain value first
+        if let data = chain.getData(key) {
+            self._value = State(initialValue: try? JSONDecoder().decode(Value.self, from: data))
+        } else if let wrappedValue = wrappedValue {
+            self._value = State(initialValue: wrappedValue)
+            
+            // Write new value
+            if let encoded = try? JSONEncoder().encode(wrappedValue) {
+                chain.set(encoded, forKey: key)
+                keychainSubject.send(key)
+            }
+        } else {
+            self._value = State(initialValue: nil)
+        }
+    }
+    
+    public func update() {
+        if let data = chain.getData(key), let decoded = try? JSONDecoder().decode(Value.self, from: data) {
+            if decoded != value {
+                value = decoded
+            }
+        } else {
+            value = nil
+        }
+    }
+    
+    public var wrappedValue: Value? {
+        get {
+            return value
+        }
+        
+        nonmutating set {
+            value = newValue
+            
+            // Delete old value
+            chain.delete(key)
+            
+            if let value = value {
+                if let encoded = try? JSONEncoder().encode(value) {
+                    chain.set(encoded, forKey: key)
+                }
+            }
+            let key = self.key
+            Task { @MainActor in
+                keychainSubject.send(key)
+            }
+        }
+    }
+    
+    public var projectedValue: Binding<Value?> {
+        get {
+            $value
+        }
+    }
+}
+
